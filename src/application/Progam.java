048835e68a7c25175ca5348e392b5aad132cff4d
Pedro G <pedro.x4vier2@gmail.com>
2025-11-05 13:12:47 -0300

    First class: Position

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Progam {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		Position pos = new Position(3, 4);
+		
+		System.out.println(pos);
 
 	}
 
