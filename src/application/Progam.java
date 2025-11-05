8cc131729ecda7ea6f2f256977f253074d36d5cd
Pedro G <pedro.x4vier2@gmail.com>
2025-11-05 13:23:46 -0300

    Starting to implement Board and Piece

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,15 +1,15 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Progam {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		
-		Position pos = new Position(3, 4);
 		
-		System.out.println(pos);
+		Board board = new Board(8, 8);
+		
+		
 
 	}
 
